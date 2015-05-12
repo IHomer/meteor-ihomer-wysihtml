491908ef62895850fe126aed385b8451b32e857a
Wim Pool <wim.pool@ihomer.nl>
2015-05-12 11:22:31 +0200

    added menu

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
-    summary: "bootstrap3 wysiwyg HTML5 editor"
+    summary: "bootstrap3 wysiwyg HTML5 editor",
+    version: "1.0.0"
 });
 
 Package.on_use(function (api) {
-    api.use(['jquery'], 'client');
+    api.use(['templating','less','jquery'], 'client');
     api.add_files('lib/wysihtml-toolbar.js', 'client', {bare:true});
     api.add_files('lib/advanced_and_extended.js', 'client', {bare:true});
+    api.add_files('lib/menuWysihtml.html', 'client');
+    api.add_files('lib/menuWysihtml.less', 'client');
     api.export(['wysihtml5'],['client']);
     api.export(['wysihtml5ParserRules'],['client']);
 });
